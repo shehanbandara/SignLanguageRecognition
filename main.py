600ae3e19a82c316905cab48695c25554466b1b6
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-11 17:48:11 -0500

    Display the prediction to the webcam
    Display the prediction to the webcam

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import tensorflow as tf
 
 # Set up the webcam
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
 
 # Load the Sign Language Recognition Model
 model  = tf.keras.models.load_model('model.h5')
@@ -17,7 +15,7 @@ dataFolders = sorted(os.listdir('SignLanguageRecognitionData/'))
 # Delete .DS_Store if present
 if dataFolders[0] == '.DS_Store':
     dataFolders.pop(0)
-    
+
 print(dataFolders)
 
 while(True):
@@ -28,6 +26,32 @@ while(True):
     # If the frame is available
     if ret == True:
         
+        # Create Region Of Interest (ROI)
+        cv2.rectangle(frame, (50, 200), (350, 500), (0, 0, 255), 5)
+        roi = frame[200:500, 50:350]
+
+        # Prepare the ROI for the prediction
+        roi = cv2.resize(roi, (50,50))
+        roi = roi / 255
+
+        # Get the prediction
+        prediction = model.predict(roi.reshape(1, 50, 50, 3))
+
+        # Get the probability of the prediction being correct
+        probability = np.amax(prediction)
+
+        # If the probability of the prediction being correct is greater than 0.9
+        if probability > 0.9:
+            
+            # Get the index of the class with the maximum probability
+            classIndex = np.argmax(prediction)
+
+            # Get the letter with the maximum probability
+            letter = dataFolders[classIndex]
+
+            # Write the letter with the maximum probability to the frame
+            cv2.putText(frame, letter, (200,180), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+
         # Display the frame
         cv2.imshow('Sign Language Recognition', frame)
 
